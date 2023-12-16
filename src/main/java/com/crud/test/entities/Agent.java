7c6f671246b01c20a465d9a9f91cc9c02fbf12bd
ManichB <153374813+ManichB@users.noreply.github.com>
2023-12-16 12:02:31 +0000

    add agent

---
+++ src/main/java/com/crud/test/entities/Agent.java
@@ -0,0 +1,54 @@
+package com.crud.test.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="agent")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Agent implements Serializable  {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private Long id;
+	
+	 
+	private String matricule;
+	
+	
+	private String nom;
+	
+	private String PostNom;
+	
+	
+	
+	private String photo;
+	
+	private String prenom;
+	
+	
+	private String sexe;
+	
+	
+	
+	
+
+}
