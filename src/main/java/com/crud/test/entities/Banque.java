15454e2ee989cc7fab825747688be74bc79b5fb1
AnyNge4545 <153374684+AnyNge4545@users.noreply.github.com>
2023-12-16 12:47:36 +0100

    Create  entities Banque

---
+++ src/main/java/com/crud/test/entities/Banque.java
@@ -0,0 +1,37 @@
+package com.crud.test.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="")
+@PrimaryKeyJoinColumn(name="idbanque")
+public class Banque implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	
+   private Long idbanque;
+	
+	private String libelle;
+	
+	private String adresse;
+
+}
